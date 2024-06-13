53a922300b3d4d00bd044a66bf868f7cf8c926f3
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-13 18:53:46 +0000

    edit_file

--- strategies/strategy.py
+++ strategies/strategy.py
@@ -3,6 +3,7 @@ import time
 from datetime import datetime
 from abc import ABC, abstractmethod
 from decouple import config
+from typing import Self, TypeVar, Generic
 
 from models.order import Order
 from models.price import Price
@@ -14,7 +15,7 @@ class Strategy(ABC):
 
     price: Price
 
-    def __init__(self, exchange, interval=60, *args, **kwargs):
+    def __init__(self: Self, exchange, interval=60, *args, **kwargs):
         self.exchange = exchange
         self._timer = None
         self.interval = interval
@@ -25,17 +26,17 @@ class Strategy(ABC):
         self.portfolio = {}
         self.test = bool(config('TRADING_MODE') != self.TRADING_MODE_REAL)
 
-    def _run(self):
+    def _run(self: Self):
         self.is_running = False
         self.start()
         self.set_price(self.exchange.symbol_ticker())
         self.run()
 
     @abstractmethod
-    def run(self):
+    def run(self: Self):
         pass
 
-    def start(self):
+    def start(self: Self):
         if not self.is_running:
             print(datetime.now())
             if self._timer is None:
@@ -47,21 +48,21 @@ class Strategy(ABC):
             self._timer.start()
             self.is_running = True
 
-    def stop(self):
+    def stop(self: Self):
         self._timer.cancel()
         self.is_running = False
 
-    def get_portfolio(self):
+    def get_portfolio(self: Self):
         self.portfolio = {'currency': self.exchange.get_asset_balance(self.exchange.currency),
                           'asset': self.exchange.get_asset_balance(self.exchange.asset)}
 
-    def get_price(self):
+    def get_price(self: Self):
         return self.price
 
-    def set_price(self, price: Price):
+    def set_price(self: Self, price: Price):
         self.price = price
 
-    def buy(self, **kwargs):
+    def buy(self: Self, **kwargs):
         order = Order(
             currency=self.exchange.currency,
             asset=self.exchange.asset,
@@ -73,7 +74,7 @@ class Strategy(ABC):
         )
         self.order(order)
 
-    def sell(self, **kwargs):
+    def sell(self: Self, **kwargs):
         order = Order(
             currency=self.exchange.currency,
             asset=self.exchange.asset,
@@ -84,11 +85,14 @@ class Strategy(ABC):
         )
         self.order(order)
 
-    def order(self, order: Order):
+    def order(self: Self, order: Order):
         print(order)
-        if self.test:
-            exchange_order = self.exchange.test_order(order)
+        try:
+            if self.test:
+                exchange_order = self.exchange.test_order(order)
+            else:
+                exchange_order = self.exchange.order(order)
+        except* Exception as e:
+            print(f"An error occurred: {e}")
         else:
-            exchange_order = self.exchange.order(order)
-
-        print(exchange_order)
+            print(exchange_order)
