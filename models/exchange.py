7af5e7823a4df39fb8c2ba2020d6a512b03a8f7f
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-14 01:28:58 +0000

    edit_file

--- models/exchange.py
+++ models/exchange.py
@@ -1,15 +1,22 @@
 from datetime import datetime
-
 from api import utils
 from models.model import AbstractModel
-
+from typing import Self
 
 class Exchange(AbstractModel):
-    resource_name = 'exchanges'
+    resource_name: str = 'exchanges'
 
     id: str = ''
     name: str = ''
 
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
 
+    def update_name(self: Self, new_name: str) -> None:
+        self.name = new_name
+
+    def get_id(self: Self) -> str:
+        return self.id
+
+    def set_id(self: Self, new_id: str) -> None:
+        self.id = new_id
