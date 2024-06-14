db7130c67c2c55c227137cc45b1579aeba5d9bc0
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-14 01:28:40 +0000

    edit_file

--- exchanges/coinbase.py
+++ exchanges/coinbase.py
@@ -1,18 +1,18 @@
 from exchanges import exchange
-
+from typing import Self
 
 class Coinbase(exchange.Exchange):
     def __init__(self, key: str, secret: str):
-        exchange.Exchange.__init__(self, key, secret)
+        super().__init__(key, secret)
         # self.client = CoinbaseAccount(self.apiKey, self.apiSecret)
         self.name = self.__class__.__name__
 
-    def get_client(self):
+    def get_client(self) -> Self:
         return self.client
 
-    def get_symbol(self):
+    def get_symbol(self) -> str:
         return self.currency + '_to_' + self.asset
 
-    def symbol_ticker(self):
+    def symbol_ticker(self) -> None:
         response = self.client.exchange_rates([self.get_symbol()])
         self.process(response)
