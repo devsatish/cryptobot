410090ed6dd9ccfe3497fd486e064a7b5e7c361a
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-14 01:28:36 +0000

    edit_file

--- services/importer.py
+++ services/importer.py
@@ -1,24 +1,30 @@
 import sys
 from datetime import datetime
 from models.dataset import Dataset
-
+from typing import Optional
 
 class Importer:
-    def __init__(self, exchange, period_start: datetime, period_end=None, interval=60, *args, **kwargs):
+    def __init__(self, exchange, period_start: datetime, period_end: Optional[datetime] = None, interval: int = 60, *args, **kwargs):
         self.exchange = exchange
         self.interval = interval
         self.period_start = period_start
         self.period_end = period_end
         self.launchedAt = datetime.now()
         self.dataset = Dataset().create(
-            data={'exchange': '/api/exchanges/'+self.exchange.name.lower(), 'periodStart': self.period_start, 'periodEnd': self.period_end,
-                  'candleSize': 60,
-                  'currency': '/api/currencies/'+self.exchange.currency.lower(), 'asset': '/api/currencies/'+self.exchange.asset.lower()})
+            data={
+                'exchange': f'/api/exchanges/{self.exchange.name.lower()}',
+                'periodStart': self.period_start,
+                'periodEnd': self.period_end,
+                'candleSize': 60,
+                'currency': f'/api/currencies/{self.exchange.currency.lower()}',
+                'asset': f'/api/currencies/{self.exchange.asset.lower()}'
+            }
+        )
 
-    def process(self):
+    def process(self) -> None:
         for price in self.exchange.historical_symbol_ticker_candle(self.period_start, self.period_end, self.interval):
-            print(price.create({'dataset': '/api/datasets/'+self.dataset.uuid}))
+            print(price.create({'dataset': f'/api/datasets/{self.dataset.uuid}'}))
 
         execution_time = datetime.now() - self.launchedAt
-        print('Execution time: ' + str(execution_time.total_seconds()) + ' seconds')
+        print(f'Execution time: {execution_time.total_seconds()} seconds')
         sys.exit(0)
