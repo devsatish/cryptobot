dda93e43f39da3269caf4ac2768febb456f0c71e
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-14 01:28:57 +0000

    edit_file

--- models/model.py
+++ models/model.py
@@ -1,9 +1,9 @@
 from api.rest import Rest
-
+from typing import Self
 
 class AbstractModel(Rest):
-    resource_name = ''
+    resource_name: str = ''
 
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs: dict[str, any]) -> None:
         for key, value in kwargs.items():
             setattr(self, key, value)
