57364d1c51659b0a923faa5eabf22cdbaec29175
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-14 01:28:51 +0000

    edit_file

--- models/currency.py
+++ models/currency.py
@@ -1,5 +1,5 @@
 from models.model import AbstractModel
-
+from typing import Self
 
 class Currency(AbstractModel):
     resource_name = 'currencies'
@@ -8,6 +8,10 @@ class Currency(AbstractModel):
     symbol: str = ''
     fiat: bool
 
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
 
+    def update_currency(self: Self, name: str, symbol: str, fiat: bool) -> None:
+        self.name = name
+        self.symbol = symbol
+        self.fiat = fiat
