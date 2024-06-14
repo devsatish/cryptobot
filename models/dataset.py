a693f90cedf4ee6dc6733435cc270b3354ac336f
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-14 01:29:03 +0000

    edit_file

--- models/dataset.py
+++ models/dataset.py
@@ -1,10 +1,9 @@
 from datetime import datetime
-
 from api import utils
 from models.model import AbstractModel
 from models.exchange import Exchange
 from models.currency import Currency
-
+from typing import Self
 
 class Dataset(AbstractModel):
     resource_name = 'datasets'
@@ -18,10 +17,24 @@ class Dataset(AbstractModel):
 
     relations = {'exchange': Exchange, 'currency': Currency, 'asset': Currency}
 
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
         self.pair = self.get_pair()
 
-    def get_pair(self):
+    def get_pair(self) -> str:
         return utils.format_pair(self.currency, self.asset)
 
+    @classmethod
+    def from_dict(cls, data: dict) -> Self:
+        instance = cls(**data)
+        return instance
+
+    def to_dict(self) -> dict:
+        return {
+            'pair': self.pair,
+            'exchange': self.exchange,
+            'period_start': self.period_start,
+            'period_end': self.period_end,
+            'currency': self.currency,
+            'asset': self.asset
+        }
